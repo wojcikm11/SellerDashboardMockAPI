887fc6e43fe501ee035315828402aadf43129316
Michał Wójcik <01153102@pw.edu.pl>
2022-01-16 15:04:19 +0100

    Dane z API zależne od Id użytkownika

--- main.py
+++ main.py
@@ -31,46 +31,50 @@ app.add_middleware(
 
 
 opinions: List[Opinion] = [
-    Opinion(id=0, rating=3, description="Produkty wystawione przez sprzedawcę takie sobie, ale za to cena nie jest taka wysoka.", date=datetime(2020, 10, 10, hour=23, minute=54, second=21)),
-    Opinion(id=1, rating=4, date=datetime(2021, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=2, rating=5, description="Cóż napisać ... jestem bardzo zadowolony. Szukałem w sieci różnych rozwiązań i trafiłem przez przypadek na tego sprzedawcę. Po zapoznaniu się z możliwościami sklepu dokonałem zakupu - cena niewielka - w sumie dużo nie ryzykowałem - brak umów więc w razie czego tylko utopione kilka złotych. Jednak się nie zawiodłem. Zarówno możliwości sklepu jak i jego obsługa posprzedaży są na najwyższym poziomie. Właśnie kupuję kolejny towar i jak najbardziej mogę wszystkim polecić. Bardzo dobra cena w stosunku do jakości.", date=datetime(2023, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=3, rating=1, description="O ile obsługa w sklepie jest ok, o tyle dział rat w arogancki i w ogóle nie wykazujący zainteresowania klientem, obsługa \"z musu\", suma sumarum zrezygnowałem z zakupu produktu, który miałem na oku. A wielka szkoda, bo długo przymierzałem się...", date=datetime(2018, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=4, rating=3, description="Jest ok", date=datetime(2019, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=5, rating=2, description="Nie jest ok", date=datetime(2017, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=6, rating=3, description="Produkty wystawione przez sprzedawcę takie sobie, ale za to cena nie jest taka wysoka.", date=datetime(2020, 10, 10, hour=23, minute=54, second=21)),
-    Opinion(id=7, rating=4, date=datetime(2021, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=8, rating=1, description="Cóż napisać ... jestem bardzo niezadowolony. Szukałem w sieci różnych rozwiązań i trafiłem przez przypadek na tego sprzedawcę. Po zapoznaniu się z możliwościami sklepu dokonałem zakupu - cena niewielka - w sumie dużo nie ryzykowałem - brak umów więc w razie czego tylko utopione kilka złotych. Jednak się nie zawiodłem. Zarówno możliwości sklepu jak i jego obsługa posprzedaży są na najwyższym poziomie. Właśnie kupuję kolejny towar i jak najbardziej mogę wszystkim polecić. Bardzo dobra cena w stosunku do jakości.", date=datetime(2023, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=9, rating=1, description="O ile obsługa w sklepie jest ok, o tyle dział rat w arogancki i w ogóle nie wykazujący zainteresowania klientem, obsługa \"z musu\", suma sumarum zrezygnowałem z zakupu produktu, który miałem na oku. A wielka szkoda, bo długo przymierzałem się...", date=datetime(2018, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=10, rating=3, description="Jest ok", date=datetime(2019, 5, 12, hour=21, minute=22, second=37)),
-    Opinion(id=11, rating=2, description="Nie jest ok", date=datetime(2017, 5, 12, hour=21, minute=22, second=37))
+    Opinion(id=0, user_id=0, rating=3, description="Produkty wystawione przez sprzedawcę takie sobie, ale za to cena nie jest taka wysoka.", date=datetime(2020, 10, 10, hour=23, minute=54, second=21)),
+    Opinion(id=1, user_id=0, rating=4, date=datetime(2021, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=2, user_id=0, rating=5, description="Cóż napisać ... jestem bardzo zadowolony. Szukałem w sieci różnych rozwiązań i trafiłem przez przypadek na tego sprzedawcę. Po zapoznaniu się z możliwościami sklepu dokonałem zakupu - cena niewielka - w sumie dużo nie ryzykowałem - brak umów więc w razie czego tylko utopione kilka złotych. Jednak się nie zawiodłem. Zarówno możliwości sklepu jak i jego obsługa posprzedaży są na najwyższym poziomie. Właśnie kupuję kolejny towar i jak najbardziej mogę wszystkim polecić. Bardzo dobra cena w stosunku do jakości.", date=datetime(2023, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=3, user_id=0, rating=1, description="O ile obsługa w sklepie jest ok, o tyle dział rat w arogancki i w ogóle nie wykazujący zainteresowania klientem, obsługa \"z musu\", suma sumarum zrezygnowałem z zakupu produktu, który miałem na oku. A wielka szkoda, bo długo przymierzałem się...", date=datetime(2018, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=4, user_id=0, rating=3, description="Jest ok", date=datetime(2019, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=5, user_id=0, rating=2, description="Nie jest ok", date=datetime(2017, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=6, user_id=0, rating=3, description="Produkty wystawione przez sprzedawcę takie sobie, ale za to cena nie jest taka wysoka.", date=datetime(2020, 10, 10, hour=23, minute=54, second=21)),
+    Opinion(id=7, user_id=0, rating=4, date=datetime(2021, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=8, user_id=0, rating=1, description="Cóż napisać ... jestem bardzo niezadowolony. Szukałem w sieci różnych rozwiązań i trafiłem przez przypadek na tego sprzedawcę. Po zapoznaniu się z możliwościami sklepu dokonałem zakupu - cena niewielka - w sumie dużo nie ryzykowałem - brak umów więc w razie czego tylko utopione kilka złotych. Jednak się nie zawiodłem. Zarówno możliwości sklepu jak i jego obsługa posprzedaży są na najwyższym poziomie. Właśnie kupuję kolejny towar i jak najbardziej mogę wszystkim polecić. Bardzo dobra cena w stosunku do jakości.", date=datetime(2023, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=9, user_id=0, rating=1, description="O ile obsługa w sklepie jest ok, o tyle dział rat w arogancki i w ogóle nie wykazujący zainteresowania klientem, obsługa \"z musu\", suma sumarum zrezygnowałem z zakupu produktu, który miałem na oku. A wielka szkoda, bo długo przymierzałem się...", date=datetime(2018, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=10, user_id=0, rating=3, description="Jest ok", date=datetime(2019, 5, 12, hour=21, minute=22, second=37)),
+    Opinion(id=11, user_id=0, rating=2, description="Nie jest ok", date=datetime(2017, 5, 12, hour=21, minute=22, second=37))
 ]
 noOpinions: List[Opinion] = []
 
-orders: Orders = Orders(unpaid=1, unsent=2, refunds=3, pending=4)
-noOrders: Orders = Orders(unpaid=0, unsent=0, refunds=0, pending=0)
+orders: Orders = Orders(user_id=0, unpaid=1, unsent=2, refunds=3, pending=4)
+noOrders: Orders = Orders(user_id=1, unpaid=0, unsent=0, refunds=0, pending=0)
+ordersMap = {
+    0: orders,
+    1: noOrders
+}
 
 
 offers: List[Offer] = [
-    Offer(id=0, photoBytes=convertImgToString("chiken.png"), name="Kurczak w sosie własnym", sold=24, turnover=59, views=103),
-    Offer(id=1, photoBytes=convertImgToString("chiken.png"), name="Pizza 4 sery - tylko u nas PROMOCJA", sold=32, turnover=21, views=224),
-    Offer(id=2, photoBytes=convertImgToString("phone.jpg"), name="Spaghetti prosto z Włoch", sold=100, turnover=2, views=42),
-    Offer(id=3, photoBytes=convertImgToString("kaczka.jpg"), name="Sushi z tego dobrego miejsca gdzie są zniżki", sold=101, turnover=103, views=13),
-    Offer(id=4, photoBytes=convertImgToString("kaczka.jpg"), name="Chińczyk", sold=6, turnover=12, views=399)
+    Offer(id=0, user_id=0, photoBytes=convertImgToString("chiken.png"), name="Kurczak w sosie własnym", sold=24, turnover=59, views=103),
+    Offer(id=1, user_id=0, photoBytes=convertImgToString("chiken.png"), name="Pizza 4 sery - tylko u nas PROMOCJA", sold=32, turnover=21, views=224),
+    Offer(id=2, user_id=0, photoBytes=convertImgToString("phone.jpg"), name="Spaghetti prosto z Włoch", sold=100, turnover=2, views=42),
+    Offer(id=3, user_id=0, photoBytes=convertImgToString("kaczka.jpg"), name="Sushi z tego dobrego miejsca gdzie są zniżki", sold=101, turnover=103, views=13),
+    Offer(id=4, user_id=0, photoBytes=convertImgToString("kaczka.jpg"), name="Chińczyk", sold=6, turnover=12, views=399)
 ]
 
 noOffers: List[Offer] = []
 
 tipsEng : List[DailyTip] =[
-    DailyTip(id=0,tip="Avoid selling low quality items."),
-    DailyTip(id=1,tip="Send orders on time. Buyers will surely appreciate your dedication and discipline."),
-    DailyTip(id=2,tip="Offer a Fair Return Policy. Buyers  look for sellers who offer a return policy as this builds trust.")
+    DailyTip(id=0, user_id=0, tip="Avoid selling low quality items."),
+    DailyTip(id=1, user_id=0, tip="Send orders on time. Buyers will surely appreciate your dedication and discipline."),
+    DailyTip(id=2, user_id=0, tip="Offer a Fair Return Policy. Buyers  look for sellers who offer a return policy as this builds trust.")
 
 ]
 
 tipsPl : List[DailyTip] =[
-    DailyTip(id=0,tip="Unikaj sprzedaży produktów o niskiej jakości."),
-    DailyTip(id=1,tip="Wysyłaj zamówienia na czas. Kupujący z pewnością docenią Twoje zaangażowanie i dyscyplinę."),
-    DailyTip(id=2,tip="Zaoferuj uczciwą politykę zwrotów. Kupujący szukają sprzedawców, którzy oferują zwroty, ponieważ buduje to zaufanie miedzy stronami.")
+    DailyTip(id=0, user_id=0, tip="Unikaj sprzedaży produktów o niskiej jakości."),
+    DailyTip(id=1, user_id=0, tip="Wysyłaj zamówienia na czas. Kupujący z pewnością docenią Twoje zaangażowanie i dyscyplinę."),
+    DailyTip(id=2, user_id=0, tip="Zaoferuj uczciwą politykę zwrotów. Kupujący szukają sprzedawców, którzy oferują zwroty, ponieważ buduje to zaufanie miedzy stronami.")
 
 ]
 
@@ -231,34 +235,59 @@ def root(id :int):
             a.append(i)
     return a
 
-@app.get("/tips/eng")
-def root():
-    return tipsEng
+@app.get("/tips/eng/{id}")
+def root(id: int):
+    tipsEngFiltered = list()
+    for i in tipsEng:
+        if i.user_id == id:
+            tipsEngFiltered.append(i)
+    return tipsEngFiltered
 
-@app.get("/tips/pl")
-async def root():
-    return tipsPl
+@app.get("/tips/pl/{id}")
+async def root(id: int):
+    tipsPlFiltered = list()
+    for i in tipsPl:
+        if i.user_id == id:
+            tipsPlFiltered.append(i)
+    return tipsPlFiltered
 
-@app.get("/opinions")
-async def root():
-    return opinions
 
-@app.get("/no_opinions")
-async def root():
-    return noOpinions
+@app.get("/opinions/{id}")
+async def root(id: int):
+    opinionsFiltered = list()
+    for i in opinions:
+        if i.user_id == id:
+            opinionsFiltered.append(i)
+    return opinionsFiltered
+
+@app.get("/no_opinions/{id}")
+async def root(id: int):
+    no_opinionsFiltered = list()
+    for i in noOpinions:
+        if i.user_id == id:
+            no_opinionsFiltered.append(i)
+    return no_opinionsFiltered
     
-@app.get("/orders")
-async def root():
-    return orders
+@app.get("/orders/{id}")
+async def root(id: int):
+    return ordersMap[id]
     
-@app.get("/no_orders")
-async def root():
-    return noOrders
+@app.get("/no_orders/{id}")
+async def root(id: int):
+    return ordersMap[id]
 
-@app.get("/offers")
-async def root():
-    return offers
+@app.get("/offers/{id}")
+async def root(id: int):
+    offers_filtered = list()
+    for i in offers:
+        if i.user_id == id:
+            offers_filtered.append(i)
+    return offers_filtered
 
-@app.get("/no_offers")
-async def root():
-    return noOffers
+@app.get("/no_offers/{id}")
+async def root(id: int):
+    no_offers_filtered = list()
+    for i in noOffers:
+        if i.user_id == id:
+            no_offers_filtered.append(i)
+    return no_offers_filtered
